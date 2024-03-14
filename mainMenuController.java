cda3faec4880f685cfc547275203727c873a6fe1
MKXL354 <mahdykamali354@yahoo.com>
2024-03-14 18:18:13 +0330

    feat: added mainMenuController

---
+++ mainMenuController.java
@@ -0,0 +1,33 @@
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+public class mainMenuController {
+    
+    @FXML
+    private Button exitButton;
+
+    @FXML
+    private Button startButton;
+
+    @FXML
+    void exitSelected(ActionEvent event) {
+        System.exit(0);
+    }
+
+    @FXML
+    void startSelected(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("FXML/difSelect.fxml"));
+        Scene scene = new Scene(root);
+        Stage primaryStage = (Stage)startButton.getScene().getWindow();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+}
